4bc0cd258637adf1246b6da0556156bca6f3855e
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2026-01-16 09:40:41 +0100

    nåt

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 from random import choice
 
 guesses_left = 8
-word_list = ["test", "hej"]
+word_list = ["tetst", "hej"]
 word = choice(word_list)
 guessed_letters = []
 guessed_word = ["_"] * len(word)
@@ -11,9 +11,34 @@ run = True
 
 while run:
     guess = input("Gissa på en bokstav: ")
-    if guess in word:
-        index = word.find(guess)
-        guessed_word[index] = guess
-        print(guessed_word)
+    guessed_letters.append(guess)
+
+    if guess in word and guesses_left > 0:
+        letter_count = word.count(guess)
+        from_index = 0
+
+        for i in range(5):
+            index = word.index(guess, from_index, len(word))
+            from_index += 1
+            guessed_word[index] = guess
+
+        #söker framifrån
+        # index = word.find(guess)
+        # guessed_word[index] = guess
+        # #söker bakifrån
+        # index2 = word.rfind(guess)
+        # guessed_word[index2] = guess
+        #söker i mitten
+        # index3 = word.find(guess, index, index2)
+        # guessed_word[index3] = guess
+
+        
+        
     else:
         print("nej")
+        guesses_left -= 1
+    
+    print(letter_count)
+    print(guessed_word)
+    print(guessed_letters)
+    print(guesses_left)
