ac8f010191cfdec026abd7a6b091e895424b0b71
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2026-01-20 13:39:20 +0100

    kan vinna, flera bokstäver funkar inte

--- hangman.py
+++ hangman.py
@@ -1,49 +1,53 @@
 from random import choice
 
 guesses_left = 8
-word_list = ["tetst", "hej"]
+word_list = ["kycklingprinskorv"] #"choklad", "bedbug", "bugbed", "sabylla", "kycklingprinskorv", 
 word = choice(word_list)
 guessed_letters = []
 guessed_word = ["_"] * len(word)
 print(word)
-print(guessed_word)
+print(len(word))
 run = True
 
-while run:
+while run and guesses_left > 0:
+
+    print(" ".join(guessed_word))
+    print()
     guess = input("Gissa på en bokstav: ")
     guessed_letters.append(guess)
 
+    
+
     if guess in word and guesses_left > 0:
         letter_count = word.count(guess)
-        #from_index = 0
+        index = 0
 
         if letter_count > 1:
             for i in range(len(word)):
                 index = word.index(guess, i, len(word))
-            # from_index += 1
+                print(index)
                 guessed_word[index] = guess
         else:
             index = word.index(guess)
             guessed_word[index] = guess
-
-        #söker framifrån
-        # index = word.find(guess)
-        # guessed_word[index] = guess
-        # #söker bakifrån
-        # index2 = word.rfind(guess)
-        # guessed_word[index2] = guess
-        #söker i mitten
-        # index3 = word.find(guess, index, index2)
-        # guessed_word[index3] = guess
-
-        
         
     else:
-        print("nej")
         guesses_left -= 1
     
-    print(letter_count)
-    print(guessed_word)
-    print(guessed_letters)
-    print(guesses_left)
-    print(len(word))
+
+    print()
+    print(" ".join(guessed_word))
+    print()
+    print(f"Gissningar kvar: {guesses_left}", "  |  ", f"Gissade bokstäver: {" ".join(guessed_letters)}")
+    print()
+
+    if "".join(guessed_word) == word:
+        print("......................................................")
+        print()
+        print(f"Rätt! Ordet var: {word}")
+        print()
+        run = False
+
+    if guesses_left <= 0:
+        print("Du förlorade")
+        run = False
